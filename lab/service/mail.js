7add9fb3217f96bc7182c5564be5420601af3316
hyang <yanghaoasian@outlook.com>
2016-05-18 17:43:31 +0800

    service mail 0.1

---
+++ lab/service/mail.js
@@ -0,0 +1,55 @@
+variable nodemailer = require('nodemailer');
+
+/**
+ * send email with mailOptions and smtpConfig
+**/
+function sendEmail(mailOptions,smtpConfig,callback){
+    // create reusable transporter object using the default SMTP transport
+    var transporter = nodemailer.createTransport(smtpConfig);
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(arguments.length == 3)
+            callback(error,info);
+    });
+}
+
+/**
+ * create the content variable
+**/
+function createMailOptions(from,to,subject,text,html){
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: from, // sender address
+        to: to, // list of receivers
+        subject: subject, // Subject line
+        text: text, // plaintext body
+        html: html // html body
+    };
+    return mailOptions;
+}
+
+/**
+ * create the smtp config variable
+**/
+function createSmtpConfig(mailserver,port,user,pass){
+    // when the user change, you should change this config
+    var smtpConfig = {
+        host: mailserver,
+        port: port,
+        //secure: true, // use SSL
+        auth: {
+            user: user,
+            pass: pass
+        }
+    };
+    return smtpConfig;
+}
+
+/**
+ * exports methods
+**/
+exports.sendEmail = sendEmail;
+exports.createMailOptions = createMailOptions;
+exports.createSmtpConfig = createSmtpConfig;
+
+
--- lab/service/mailtest.js
+++ lab/service/mailtest.js
@@ -1,46 +1,31 @@
-var nodemailer = require('nodemailer');
+var mail = require('./mail.js');
 
-var user = "gitnovel@outlook.com";//"iceashsky@gmail.com";//"gitnovel@gmail.com";//"gitnovel@qq.com";
-var pass = "scucs2009";//"wadashinoGMAIL!9";//
-var to = "hyang.cn@outlook.com, 250553694@qq.com";
 
 // when the user change, you should change this config
-var outlookSmtpConfig = {
-    host: 'smtp-mail.outlook.com',
-    port: 587,
-    //secure: true, // use SSL
-    auth: {
-        user: user,
-        pass: pass
-    }
-};
-
-var gmailSmtpConfig = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    //secure: true, // use SSL
-    auth: {
-        user: user,
-        pass: pass
-    }
-};
-
-// create reusable transporter object using the default SMTP transport
-var transporter = nodemailer.createTransport(outlookSmtpConfig);
+var mailserver = 'smtp-mail.outlook.com';
+var port = 587;
+var user = "gitnovel@outlook.com";//"iceashsky@gmail.com";//"gitnovel@gmail.com";//"gitnovel@qq.com";
+var pass = "scucs2009";//"wadashinoGMAIL!9";//
+var smtpConifg = mail.createSmtpConfig(mailserver,port,user,pass);
 
 // setup e-mail data with unicode symbols
-var mailOptions = {
-    from: '"GitNovel Official Service" ' + user, // sender address
-    to: to, // list of receivers
-    subject: 'Test of Nodemailer', // Subject line
-    text: 'Hello world', // plaintext body
-    html: '<b>Test the html 🐴</b>' // html body
-};
+var from = '"GitNovel Official Service" ' + user;
+var to = "hyang.cn@outlook.com, 250553694@qq.com";
+var subject = 'Test of our mail helper: callback ';
+var text = 'Hello world，wow!!!';
+var html = '<b>Html  body file 🐴</b>';
+var mailOptions = mail.createMailOptions(from,to,subject,text,html);
 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
+// callback function for the result of send mail
+function mailCallback(error,info){
     if(error){
-        return console.log(error);
+        console.log("Error: Mail send error");
     }
-    console.log('Message sent: ' + info.response);
-});
+    else
+        console.log("Mail send success");
+}
+
+// send email 
+mail.sendEmail(mailOptions,smtpConifg,mailCallback);    
+// mail.sendEmail(mailOptions,smtpConifgs);
+
